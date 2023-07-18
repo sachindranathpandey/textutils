4fdb0406503856279056007db45605196e1383ee
Sachindra Pandey <sachindra>
2023-07-19 01:44:51 +0530

    first-commit

---
+++ src/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <div className="nav">
+    <header className="App-header">
+     
+    <h1>This is App6.js</h1>
+    </header>
+  </div>
+  );
+}
+
+export default Nav;
